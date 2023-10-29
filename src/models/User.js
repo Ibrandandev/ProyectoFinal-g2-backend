89cfb645db13562bc2283e2942af8f5c46342ea6
Ignacio Brandán <brandanignaciod@gmail.com>
2023-10-28 21:01:24 -0300

    Creo Modelo de Usuario

---
+++ src/models/User.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = Schema({
+  nombre: { type: String, required: [true, "El nombre es requerido"] },
+  apellido: { type: String, required: [true, "El apellido es requerido"] },
+  email: {
+    type: String,
+    required: [true, "El email es requerido"],
+    unique: true,
+  },
+  telefono: { type: String },
+  planContratado: { type: String },
+  password: { type: String, required: [true, "La contraseña es requerida"] },
+  imagen: { type: String },
+  rol: {
+    type: String,
+    required: true,
+  },
+  usuarioActivo: { type: Boolean, default: true },
+});
+
+UserSchema.methods.toJSON = function () {
+  const { __v, password, ...user } = this.toObject();
+
+  return user;
+};
+
+module.exports = model("User", UserSchema);

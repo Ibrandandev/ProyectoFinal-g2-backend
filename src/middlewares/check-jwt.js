d37ee62e1c98e38f33784659a197c49618ccb9f2
Ignacio Brandán <brandanignaciod@gmail.com>
2023-11-02 01:37:32 -0300

    Agrego Creacion y validacion JWT

---
+++ src/middlewares/check-jwt.js
@@ -0,0 +1,37 @@
+const { request, response } = require("express");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const checkJWT = async (req = request, res = response, next) => {
+  const token = req.header("token");
+
+  if (!token) {
+    return res.status(401).json({ message: "El token es requerido" });
+  }
+
+  try {
+    const { userId } = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "El token no es valido, el usuario no existe!" });
+    }
+
+    if (!user.usuarioActivo) {
+      return res.status(401).json({
+        message: "El token no es valido, el usuario esta deshabilitado!",
+      });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (err) {
+    console.log(err);
+    return res.status(401).json({ message: "El token no es valido" });
+  }
+};
+
+module.exports = { checkJWT };

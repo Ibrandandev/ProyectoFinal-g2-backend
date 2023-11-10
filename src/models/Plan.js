8759bdf199eabbd0a892d9580c39329f56b13a77
Ignacio Brandán <brandanignaciod@gmail.com>
2023-11-09 21:34:33 -0300

    Actualizacion de modelo de Plan

--- src/models/Plan.js
+++ src/models/Plan.js
@@ -6,9 +6,15 @@ const PlanSchema = Schema({
     required: [true, "El nombre del plan es requerido"],
     unique: true,
   },
-  beneficios: { type: Array },
+  duracion: {
+    type: String,
+    required: [true, "La duracion del plan es requerida"],
+  },
   precio: { type: Number, required: [true, "EL Precio es requerido"] },
+  img: { type: String, required: [true, "La imagen es requerida"] },
   descripcion: { type: String },
+  beneficios: { type: Array },
+  estado: { type: Boolean, default: true },
 });
 
 module.exports = model("Plan", PlanSchema);

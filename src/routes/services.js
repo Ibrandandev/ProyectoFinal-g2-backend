1b6f51b0e60fa68e88c4ae18ffd4e21bd406c5e9
Ignacio Brandán <brandanignaciod@gmail.com>
2023-11-13 16:47:10 -0300

    Agrego validaciones a ruta POST de Servicios

--- src/routes/services.js
+++ src/routes/services.js
@@ -6,12 +6,32 @@ const {
   putService,
   deleteService,
 } = require("../controllers/servicesCtrl");
+const { check } = require("express-validator");
+const { checkFields } = require("../middlewares/checkFields");
+const { checkJWT } = require("../middlewares/check-jwt");
+const { isAdminRole } = require("../middlewares/checkRoles");
 
 const router = Router();
 
 router.get("/", [], getServices);
 router.get("/:id", [], getService);
-router.post("/", [], postService);
+router.post(
+  "/",
+  [
+    checkJWT,
+    isAdminRole,
+    check("nombre", "El nombre es requerido").notEmpty(),
+    check("categoria", "La categoria es requerida").notEmpty(),
+    check("profesor", "El profesor es requerido").notEmpty(),
+    check("descripcion", "La descripcion es requerida").notEmpty(),
+    check("dias", "La descripcion es requerida").notEmpty(),
+    check("horario", "El horario es requerido").notEmpty(),
+    check("img", "La imagen es requerida").notEmpty(),
+    check("cupo", "El numero de participantes es requerido").notEmpty(),
+    checkFields,
+  ],
+  postService
+);
 router.put("/:id", [], putService);
 router.delete("/:id", [], deleteService);
 

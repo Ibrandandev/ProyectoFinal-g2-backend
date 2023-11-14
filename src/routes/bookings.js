db7a0d372dcaf66a82eda943e99cce944f904cb6
Ignacio Brandán <brandanignaciod@gmail.com>
2023-11-14 12:52:03 -0300

    Agrego validaciones a ruta de reservas

--- src/routes/bookings.js
+++ src/routes/bookings.js
@@ -5,12 +5,20 @@ const {
   postBooking,
   deleteBooking,
 } = require("../controllers/BookingsCtrl");
+const { checkJWT } = require("../middlewares/check-jwt");
+const { isAdminRole } = require("../middlewares/checkRoles");
+const { checkFields } = require("../middlewares/checkFields");
+const { check } = require("express-validator");
 
 const router = Router();
 
-router.get("", [], getBookings);
-router.get("", [], getBookingsByUser);
-router.post("", [], postBooking);
-router.delete("", [], deleteBooking);
+router.get("/", [checkJWT, isAdminRole], getBookings);
+router.get("/:user", [checkJWT], getBookingsByUser);
+router.post(
+  "/",
+  [checkJWT, check("servicio", "El servicio es requerido"), checkFields],
+  postBooking
+);
+router.delete("/:id", [checkJWT], deleteBooking);
 
 module.exports = router;

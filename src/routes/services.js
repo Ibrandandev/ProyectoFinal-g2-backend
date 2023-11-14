5bf23e56d4e9e1dc4dd34fac296510d956552bfe
Ignacio Brandán <brandanignaciod@gmail.com>
2023-11-14 12:54:14 -0300

    Agrego Validacion de ruta PUT de servicios

--- src/routes/services.js
+++ src/routes/services.js
@@ -32,7 +32,23 @@ router.post(
   ],
   postService
 );
-router.put("/:id", [], putService);
+router.put(
+  "/:id",
+  [
+    checkJWT,
+    isAdminRole,
+    check("nombre", "El nombre es requerido").notEmpty(),
+    check("categoria", "La categoria es requerida").notEmpty(),
+    check("profesor", "El profesor es requerido").notEmpty(),
+    check("descripcion", "La descripcion es requerida").notEmpty(),
+    check("dias", "La descripcion es requerida").notEmpty(),
+    check("horario", "El horario es requerido").notEmpty(),
+    check("img", "La imagen es requerida").notEmpty(),
+    check("cupo", "El numero de participantes es requerido").notEmpty(),
+    checkFields,
+  ],
+  putService
+);
 router.delete("/:id", [], deleteService);
 
 module.exports = router;
